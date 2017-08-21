762304e5a47fbc69ce44d5c2d53b5e7b6c43ce89
allengzh <18862242813@139.com>
2017-08-21 14:53:28 +0800

    md

--- juf-conf.js
+++ juf-conf.js
@@ -1,25 +1,23 @@
 var fs = require('fs');
 var static = 'assets';
 var template = 'views';
-var test = 'test';
-var map = 'map';
 var devTemplate = 'themes';
 var devStatic = 'static';
 
 var config = {
-    namespace: 'demo',   // 整个模块编译的命名空间
+    namespace: 'demo',      // 整个模块编译的命名空间
     type: 'smarty',         // 指定编译继承哪个解决方案的默认配置
     // 编译到本地服务器配置，default，对应编译命令 juf release
     default: {
         tpl: {
             src: '**/*.tpl',
             deploy: template,
-            useDir: false   // smarty解决方案有模版指定目录，在前端资源引入的时候不需要带上目录
+            useDir: false   // 由于基于smarty的解决方案本地服务器有指定模版访问目录，所以在代码中资源引入的时候不需要加上模版目录
         },
         js: {
             src: '**/*.js',
             deploy: static,
-            useDir: true    // smarty解决方案没有指定静态资源目录，在前端资源引入的时候需要带上目录
+            useDir: true    // 由于基于smarty的解决方案本地服务器没有指定静态访问目录，所以在代码中资源引入的时候需要加上静态访问目录
         },
         less: {
             src: '**/*.less',
@@ -37,12 +35,12 @@ var config = {
        tpl: {
             src: '**/*.tpl',
             deploy: devTemplate,
-            useDir: false
+            useDir: false   // 由于基于smarty的解决方案线上服务器有指定模版访问目录，所以在代码中资源引入的时候不需要加上模版目录
         },
         js: {
             src: '**/*.js',
             deploy: devStatic,
-            useDir: true
+            useDir: true   // 由于基于smarty的解决方案线上服务器没有指定静态访问目录，所以在代码中资源引入的时候需要加上静态访问目录
         },
         less: {
             src: '**/*.less',
